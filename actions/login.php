439ac1d16043e98ff772d8a7367a72ea0b951ab5
iamtheluiz <iamtheluiz.dev@gmail.com>
2018-10-01 02:15:03 -0300

    Adicionada função de login

---
+++ actions/login.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<?php
+
+include_once('../class/Sys.php');
+$sys = new Sys;
+$pdo = $sys->getPdo();
+
+if(isset($_POST['login']) and isset($_POST['pass']) and !empty($_POST['login']) and !empty($_POST['pass'])){
+
+	$login = $_POST['login'];
+	$pass = $_POST['pass'];
+
+	$sys->login($login,$pass);
+
+}else{
+	$sys->redirect("Prencha o formulário para acessar essa página!","../index.php");
+}
